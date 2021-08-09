562518e6a7877d774ac515e83f6beff09b04cd28
Matpay1 <mateo120@live.fr>
2021-08-09 06:13:45 +0200

    0.2

--- app/src/main/java/com/example/quizz/MainActivity.kt
+++ app/src/main/java/com/example/quizz/MainActivity.kt
@@ -11,46 +11,110 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
-    var num = 1
-    var score = 0
-    fun numpag() {
-        val num_quest = findViewById<TextView>(R.id.num_quest)
-        num ++
-        num_quest.text = "n°" + num
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val question = findViewById<TextView>(R.id.question)
-        val bt_1 = findViewById<Button>(R.id.bt_1)
-        val bt_2 = findViewById<Button>(R.id.bt_2)
-        val bt_3 = findViewById<Button>(R.id.bt_3)
-        val bt_4 = findViewById<Button>(R.id.bt_4)
+        var bt_1 = findViewById<Button>(R.id.bt_1)
+        var bt_2 = findViewById<Button>(R.id.bt_2)
+        var bt_3 = findViewById<Button>(R.id.bt_3)
+        var bt_4 = findViewById<Button>(R.id.bt_4)
         val resultat1 = findViewById<TextView>(R.id.resultat1)
         val bt_ret = findViewById<Button>(R.id.bt_ret)
+        var bt_0 = "rien"
+        var score = 0
 
-        fun bon_rep(){
-            resultat1.text = "  bonne  réponse"
-            resultat1.setTextColor(Color.parseColor("#4CAF50"))
+        var ok = 0
+
+        fun bon_rep () {
+            resultat1.text = "  bonne  réponse"+ok
+            resultat1.setTextColor(Color.parseColor("#19DD21"))
             score ++
         }
-        fun mauv_rep(){
-            resultat1.text = "mauvaise réponse"
+
+        fun mauv_rep () {
+            resultat1.text = "mauvaise réponse"+ok
             resultat1.setTextColor(Color.parseColor("#F44336"))
         }
 
+        fun quest_2 () {
+            question.text = "Qui chante \"Il en faut peu pour être heureux...\" ? "
+            bt_1.text = "Simba"
+            bt_2.text = "Baloo"
+            bt_3.text = "toi"
+            bt_4.text = "Baghera"
+        }
+        fun quest_3 () {
+            question.text = "Quel métier exerce l'ami de Mary Poppins ? "
+            bt_1.text = "Plombier "
+            bt_2.text = "Ramoneur"
+            bt_3.text = "Couvreur"
+            bt_4.text = "Peintre"
+            bt_3.setOnClickListener {
+                bon_rep()
+            }
+        }
+        fun quest_4 () {
+            question.text = "Qui fume sur un champignon devant Alice ? "
+            bt_1.text = "Une abeille"
+            bt_2.text = "Une chenille"
+            bt_3.text = "Un papillon"
+            bt_4.text = "Un ver"
 
+        }
+        fun quest_5 () {
+            question.text = "Lequel de ces ustensiles n'est pas utilisé dans le piège qui doit tuer Basil ?"
+            bt_1.text = "Un phonographe"
+            bt_2.text = "Un couteau"
+            bt_3.text = "Un arbalète"
+            bt_4.text = "Une hache"
+        }
+        fun quest_6 () {
+            question.text = "Quelle profession exerce Basil ?"
+            bt_1.text = "Constructeur de limes à épaissir"
+            bt_2.text = "Inspecteur de police"
+            bt_3.text = "Commissaire de police"
+            bt_4.text = "Détective privé"
+        }
+        fun quest_7 () {
+            question.text = "Dans quel véhicule Edgar enlève-t'il les chatons de Duchesse ?"
+            bt_1.text = "Dans une fourgonnette"
+            bt_2.text = "Dans une voiture"
+            bt_3.text = "Dans un fiacre"
+            bt_4.text = "Dans un side-car"
+        }
+        fun quest_8 () {
+            question.text = " Comment s'appelle le petit singe d'Aladdin ?"
+            bt_1.text = "Aba"
+            bt_2.text = "Abi"
+            bt_3.text = "Abo"
+            bt_4.text = "Abu"
+        }
+        fun quest_9 () {
+            question.text = " Que prend Ursula, la sorcière de la mer à Ariel, la petite sirène ?"
+            bt_1.text = "Sa jeunesse"
+            bt_2.text = "Sa beauté"
+            bt_3.text = "Sa voix"
+            bt_4.text = "Son scooter des mers"
+        }
+        fun quest_10 () {
+            question.text = "Qui chante \"Hakuna Matata\" ?"
+            bt_1.text = "Pimba"
+            bt_2.text = "Samba"
+            bt_3.text = "Pumba"
+            bt_4.text = "Bamba"
+        }
 
-        fun bien_jouet(){
+        fun bien_jouet () {
             question.text = "vous avez bien jouer recommencer "
             bt_1.text = "c'etait"
             bt_2.text = "une"
             bt_3.text = "bonne"
             bt_4.text = "partie"
             AlertDialog.Builder(this)
-                .setTitle("bien jouer")
-                .setMessage("je vous invite a redemarer" +
-                        " votre score est de "+score)
+                .setTitle("BIEN JOUER")
+                .setMessage("Je vous invite à redémarer                          votre score est de "+score+"/10")
                 .setPositiveButton(android.R.string.yes) { dialog, which ->
                     val newIntent = Intent(this, MainActivity::class.java)
                     startActivity(newIntent)
@@ -59,132 +123,94 @@ class MainActivity : AppCompatActivity() {
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show()
         }
-        fun newquest4(){
-            question.text = "Qui fume sur un champignon devant Alice ? "
-            bt_1.text = "Une abeille"
-            bt_2.text = "Un ver"
-            bt_3.text = "Un papillon"
-            bt_4.text = "Une chenille "
+        fun ques_alea() {
+            /*bt_0 = bt_1.text as String
+            bt_1.text = bt_3.text
+            bt_3.text = bt_4.text
+            bt_4.text = bt_2.text
+            bt_2.text = bt_0*/
+            var x = bt_3
 
-            bt_1.setOnClickListener {
-                mauv_rep()
-                bien_jouet()
-                numpag()
-            }
-            bt_2.setOnClickListener {
-                mauv_rep()
-                bien_jouet()
-                numpag()
-            }
-            bt_3.setOnClickListener{
-                mauv_rep()
-                bien_jouet()
-                numpag()
-            }
-            bt_4.setOnClickListener {
-                bon_rep()
-                bien_jouet()
-                numpag()
-            }
+            /*x    = bt_2
+            bt_2 = bt_1
+            bt_1 = bt_3
+            bt_3 = bt_4
+            bt_4 = x*/
+
         }
-        fun newquest3(){
-            question.text = "Quel métier exerce l'ami de Mary Poppins ? "
-            bt_1.text = "Plombier "
-            bt_2.text = "Couvreur"
-            bt_3.text = "Ramoneur"
-            bt_4.text = "Peintre"
-
-            bt_1.setOnClickListener {
-                mauv_rep()
-                resultat1.setTextColor(Color.parseColor("#F44336"))
-                newquest4()
-                numpag()
-            }
-            bt_2.setOnClickListener {
-                mauv_rep()
-                newquest4()
-                numpag()
-            }
-            bt_3.setOnClickListener{
+        fun ok (){
+            if (ok <= 4){
                 bon_rep()
-                newquest4()
-                numpag()
             }
-            bt_4.setOnClickListener{
+            else{
                 mauv_rep()
-                newquest4()
-                numpag()
             }
         }
 
-        fun newquest2(){
-            question.text = "Qui chante \"Il en faut peu pour être heureux...\" ? "
-            bt_1.text = "Simba"
-            bt_2.text = "Baghera"
-            bt_3.text = "toi"
-            bt_4.text = "Baloo"
 
-            bt_1.setOnClickListener {
-                mauv_rep()
-                newquest3()
-                numpag()
+        var num = 1
+
+        fun numpag() {
+            val num_quest = findViewById<TextView>(R.id.num_quest)
+            num ++
+            num_quest.text = "n°" + num
+
+            if (num == 2){
+                quest_2()
             }
-            bt_2.setOnClickListener {
-                mauv_rep()
-                newquest3()
-                numpag()
+            else if (num == 3){
+                quest_3()
             }
-            bt_3.setOnClickListener{
-                mauv_rep()
-                newquest3()
-                numpag()
+            else if (num == 4){
+                quest_4()
             }
-            bt_4.setOnClickListener{
-                bon_rep()
-                newquest3()
-                numpag()
+            else if (num == 5){
+                quest_5()
             }
+            else if (num == 6){
+                quest_6()
+            }
+            else if (num == 7){
+                quest_7()
+            }
+            else if (num == 8){
+                quest_8()
+            }
+            else if (num == 9){
+                quest_9()
+            }
+            else if (num == 10){
+                quest_10()
+            }
+            else
+                bien_jouet()
         }
-
         bt_1.setOnClickListener{
-            bon_rep()
-            newquest2()
+            mauv_rep()
             numpag()
+            ques_alea()
         }
         bt_2.setOnClickListener{
-            mauv_rep()
-            newquest2()
+            /*bon_rep()*/
             numpag()
+            ques_alea()
         }
         bt_3.setOnClickListener{
             mauv_rep()
-            newquest2()
             numpag()
+            ques_alea()
         }
         bt_4.setOnClickListener{
             mauv_rep()
-            newquest2()
             numpag()
+            ques_alea()
         }
+
 
         bt_ret.setOnClickListener {
             val newIntent = Intent(this, MainActivity::class.java)
             startActivity(newIntent)
         }
-
-
-
-
-
-        /*fun reponce () {
-            if (et_c1.text.isNotEmpty() and et_c2.text.isNotEmpty()) {
-                tvmess.text = "valide"
-            } else if (et_c1.text.isNotEmpty() or et_c2.text.isNotEmpty()) {
-                tvmess.text = "veuillez remplir l'autre champs"
-            } else {
-                tvmess.text = "veuillez remplir les champs"
-            }
-        }*/
     }
 
 }
